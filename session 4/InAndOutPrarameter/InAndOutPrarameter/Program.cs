751af2d840b7f48dee2ccf6036d8a9522102c784
iamsantosh007 <110806378+iamsantosh007@users.noreply.github.com>
2023-01-23 10:59:03 +0530

    Session 4 Assignment

---
+++ session 4/InAndOutPrarameter/InAndOutPrarameter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InAndOutPrarameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName;                             //use of out parameter
+            GetName("santosh","patro",out fullName);
+            Console.WriteLine(fullName);
+            Console.WriteLine("----------------------");
+
+            bool result=false;                            //use of out,in and int.TryParse()
+            int countryCode = 91;
+            int mobileNumber;
+            string fullMobileNum;
+            GetUserNumber(out mobileNumber, result);
+            GetFullMobileNumber(countryCode,out fullMobileNum,mobileNumber);
+            Console.WriteLine(fullMobileNum);
+            Console.ReadLine();
+        }
+        public static void GetName(string firstName,string lastName,out string fullName)
+        {
+            fullName = firstName + " " + lastName;
+        }
+
+        public static void GetUserNumber(out int mobileNumber,bool result)
+        {
+            Console.Write("enter your number:- ");
+            
+            result = int.TryParse(Console.ReadLine(),out mobileNumber);
+            
+            
+        }
+
+        public static void GetFullMobileNumber(in int countryCode,out string fullNumber,in int mobileNum)
+        {
+            fullNumber=countryCode.ToString()+mobileNum.ToString();
+
+        }
+
+    }
+}
---
+++ session 4/ParamArray/ParamArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamArray
+{
+    class Program
+    {
+        public static void PrintSum(params int[] num)
+        {
+            string count = num.Length > 5 ? "you enter above than 5 number" : "";
+            int sum = 0;
+            foreach (int i in num)
+            {
+                sum+= i;
+            }
+            if(count!=null)
+                Console.WriteLine(count);
+            Console.WriteLine($"The sum of number is {sum}");
+
+        }
+        static void Main(string[] args)
+        {
+            PrintSum(5, 7, 8);
+            Console.ReadLine();
+        }
+    }
+}
