9a5e430ab3c6e9e37966408d8bca8b7d2c3afbcf
iamsantosh007 <110806378+iamsantosh007@users.noreply.github.com>
2023-01-30 10:20:50 +0530

    session 8 assignment

---
+++ session 8/EvenOddApp/EvenOddApp/Models/EvenOdd.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOddApp.Models
+{
+    class EvenOdd
+    {
+        private int[] _evenNum;
+        private int[] _oddNum;
+
+        public  int[] EvenNum { 
+            get { return _evenNum; } 
+            set { _evenNum = value; } 
+        }
+
+        public int[] OddNum { get { return _oddNum; } set { _oddNum = value; } }
+
+        public void EvenNumGenerator(int range)
+        {
+            int indexNum = 0;
+            EvenNum = new int[range/2];
+            for(int i=0;i<range; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    EvenNum[indexNum] = i;
+                    indexNum++;
+                }
+            }
+            
+        }
+
+        public void OddNumGenerator(int range)
+        {
+            int indexNum = 0;
+            OddNum = new int[range / 2];
+            for (int i = 0; i < range; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    OddNum[indexNum] = i;
+                    indexNum++;
+                }
+            }
+           
+        }
+    }
+}
---
+++ session 8/EvenOddApp/EvenOddApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using EvenOddApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOddApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EvenOdd evenOdd = new EvenOdd(); 
+            Console.WriteLine("enter the range for Even And Odd");
+            int range=Convert.ToInt32(Console.ReadLine());
+            evenOdd.EvenNumGenerator(range);
+            evenOdd.OddNumGenerator(range);
+            Console.WriteLine("Even number for range "+range);
+            foreach(int i in evenOdd.EvenNum)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("odd num for range "+range);
+            foreach(int i in evenOdd.OddNum)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
