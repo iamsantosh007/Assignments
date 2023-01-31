36f77450c6d3c6169175046752db54f933dfe41a
iamsantosh007 <110806378+iamsantosh007@users.noreply.github.com>
2023-01-31 10:08:00 +0530

    session 9 assignment

---
+++ session 9/AppConfigExample/AppConfigExample/Models/AppConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConfigExample.Models
+{
+    public class AppConfig
+    {
+        public void ConnectionTwo()
+        {
+            string connectionTwo = ConfigurationSettings.AppSettings["connection2"];
+            string serverName = connectionTwo.Split(';')[0];
+            Console.WriteLine(serverName);
+
+            string dataBaseName = connectionTwo.Split(';')[1];
+            Console.WriteLine(dataBaseName);
+        }
+
+        public void ConnectionOne()
+        {
+            string connectionOne = ConfigurationSettings.AppSettings["connection1"];
+            string serverName = connectionOne.Split(';')[0];
+            Console.WriteLine(serverName);
+
+            string dataBaseName = connectionOne.Split(';')[1];
+            Console.WriteLine(dataBaseName);
+        }
+    }
+}
---
+++ session 9/AppConfigExample/AppConfigExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using AppConfigExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConfigExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppConfig appConfig=new AppConfig();
+            Console.WriteLine("server name for connection one:-");
+            appConfig.ConnectionOne();
+            Console.WriteLine();
+            Console.WriteLine("server name for connection two:-");
+            appConfig.ConnectionTwo();  
+
+            //Console.WriteLine("connection details for one"+connectionOne);
+            //Console.WriteLine();
+            //Console.WriteLine("connection details for Two"+connectionTwo);
+        }
+
+        
+
+        
+    }
+}
