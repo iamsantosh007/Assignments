00591913ef7eb839304e4f01f95784cb2d1b8086
iamsantosh007 <110806378+iamsantosh007@users.noreply.github.com>
2023-02-23 14:48:09 +0530

    session assignment

--- session 26/BoardGame/BoardGame/Program.cs
+++ session 26/BoardGame/BoardGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BoardGame.Models;
+using System.Numerics;
 
 namespace BoardGame
 {
@@ -11,28 +12,30 @@ namespace BoardGame
             string[,] board = new string[3, 3];
             bool player1 = true;
             bool player2 = false;
-            StartGame(player1,player2,board,p1,p2);
+            StartGame(player1, player2, board, p1, p2);
         }
-        private static void StartGame(bool player1,bool player2, string[,] board,Player p1,Player p2)
+        private static void StartGame(bool player1, bool player2, string[,] board, Player p1, Player p2)
         {
+            //Console.WriteLine("\t\t\t\t----first enter the row betweern 0 to 2 and column bewteen 0 to 2----\n" +
+            //    "\t\t\t\t----E.g:-0 2 will put your input in 1st row and 3rd column----\n\n");
             while (player1 || player2)
             {
                 int remainingFields = ShowBoard(board);
                 if (remainingFields == board.Length)
                 {
                     Console.WriteLine("\t\t\t-----***No fields Remaining to play***---------");
-                    PlayAgain(player1,player2,board,p1,p2);
+                    PlayAgain(player1, player2, board, p1, p2);
                     return;
                 }
                 //Player curruntPlayer= null;
                 if (player1)
                 {
-                    FillTheBoxes(p1, board, ref player1, ref player2,p2);
+                    FillTheBoxes(p1, board, ref player1, ref player2, p2);
                     continue;
 
                 }
 
-                FillTheBoxes(p2, board, ref player1, ref player2,p2);
+                FillTheBoxes(p2, board, ref player1, ref player2, p2);
 
 
             }
@@ -41,10 +44,10 @@ namespace BoardGame
         private static int ShowBoard(string[,] board)
         {
             int count = 0;
-           
+
             for (int i = 0; i < 3; i++)
             {
-                count+=ContentInsideEachRow(i, board);
+                count += ContentInsideEachRow(i, board);
                 Console.WriteLine();
             }
             return count;
@@ -69,37 +72,62 @@ namespace BoardGame
             return elementCount;
         }
 
+        private static bool SwitchFunc(int input, string[,] board,Player player)
+        {
+            switch (input)
+            {
+                case 1:
+                    board[0, 0] = player.InputType();
+                    break;
+                case 2:
+                    board[0, 1] = player.InputType();
+                    break;
+                case 3:
+                    board[0, 2] = player.InputType();
+                    break;
+                case 4:
+                    board[1, 0] = player.InputType();
+                    break;
+                case 5:
+                    board[1, 1] = player.InputType();
+                    break;
+                case 6:
+                    board[1, 2] = player.InputType();
+                    break;
+                case 7:
+                    board[2, 0] = player.InputType();
+                    break;
+                case 8:
+                    board[2, 1] = player.InputType();
+                    break;
+                case 9:
+                    board[2, 2] = player.InputType();
+                    break;
+                default:
+                    Console.WriteLine("please enter in correct input");
+                    return false;
+                    
 
-        private static void FillTheBoxes(Player player, string[,] board, ref bool player1BoolValue, ref bool player2BoolValue,Player player2)
+            }
+            return true;
+        }
+        private static void FillTheBoxes(Player player, string[,] board, ref bool player1BoolValue, ref bool player2BoolValue, Player player2)
         {
             bool isGreater = false;
-            Console.WriteLine("\t\t\t\t-----**"+player.Name + " your turn**---------");
-            Console.Write("Enter your value by providing the row and column number:-");
-            string input = Console.ReadLine();
-            string[] num = input.Split(" ");
-            int row = Convert.ToInt32(num[0]);
-            int column = Convert.ToInt32(num[1]);
-            while(row > 2 || column > 2)
-            {
-                Console.WriteLine("\n\n-------please provide correct position-----------");
-                Console.Write("Enter your value by providing the row and column number:-");
-                input = Console.ReadLine();
-                num = input.Split(" ");
-                row = Convert.ToInt32(num[0]);
-                column = Convert.ToInt32(num[1]);
-            }
-            while (board[row, column] != null)
+            Console.WriteLine("\t\t\t\t-----**" + player.Name + " your turn**---------");
+            Console.Write("Enter the position number:- ");
+            int input = Convert.ToInt32(Console.ReadLine());
+       
+           
+            while (SwitchFunc(input, board, player)==false)
             {
                 //if (row>2 && column>2) Console.WriteLine("please provide correct position");
                 Console.WriteLine("\t\t\t-----This place is Already filled---------");
-                Console.Write("Enter your value by providing the row and column number:-");
-                input = Console.ReadLine();
-                num = input.Split(" ");
-                row = Convert.ToInt32(num[0]);
-                column = Convert.ToInt32(num[1]);
+                Console.Write("Enter the position number:- ");
+                input = Convert.ToInt32(Console.ReadLine());
             }
-            board[row, column] = player.InputType();
-            if (Wingame(player, board,ref player1BoolValue,ref player2BoolValue,player2))
+            //board[row, column] = player.InputType();
+            if (Wingame(player, board, ref player1BoolValue, ref player2BoolValue, player2))
             {
 
                 player1BoolValue = false;
@@ -116,23 +144,24 @@ namespace BoardGame
         private static void PlayAgain(bool player1, bool player2, string[,] board, Player p1, Player p2)
         {
             Console.WriteLine("-----------------Press y for play and press n to exit--------------");
-            char option=Convert.ToChar(Console.ReadLine().ToLower());
+            char option = Convert.ToChar(Console.ReadLine().ToLower());
             switch (option)
             {
                 case 'y':
-                    player1= true;
-                    player2= false;
+                    player1 = true;
+                    player2 = false;
                     board = new string[3, 3];
+                    Console.WriteLine("\n\n\n\n");
                     StartGame(player1, player2, board, p1, p2);
                     break;
                 case 'n':
                     Console.WriteLine("\t\t\t\t---Good bye---");
                     break;
-                    
+
             }
         }
 
-        private static bool Wingame(Player player, string[,] board,ref bool player1BoolValue,ref bool player2BoolValue,Player player2)
+        private static bool Wingame(Player player, string[,] board, ref bool player1BoolValue, ref bool player2BoolValue, Player player2)
         {
             string playerInputType = player.InputType();
             if ((playerInputType == board[0, 0] && playerInputType == board[0, 1] && playerInputType == board[0, 2]) ||
@@ -146,7 +175,7 @@ namespace BoardGame
             {
                 ShowBoard(board);
                 Console.WriteLine($"\t\t----**Bingo {player.Name} You win the gane**----");
-                
+
                 return true;
             }
             return false;
