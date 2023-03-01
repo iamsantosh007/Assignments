7467769738a7bebce604e81a9d016af0032cc531
iamsantosh007 <110806378+iamsantosh007@users.noreply.github.com>
2023-03-01 11:00:55 +0530

    session assignment

--- session 30/EmployeeApp/EmployeeApp/Program.cs
+++ session 30/EmployeeApp/EmployeeApp/Program.cs
@@ -9,14 +9,97 @@
             CaseStudyForGeneratingEmployingByDesignation();
             CaseStudyForGeneratingEmployingById();
             CaseStudyForGeneratingEmployingBySalary();
+           CaseStudyForGeneratingEmployingByIdAndDesignation();
             
 
 
         }
 
+        private static void CaseStudyForGeneratingEmployingByIdAndDesignation()
+        {
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.WriteLine("--------------------Employee With Same Designation And Same DeptId-------------------\n");
+            Console.ResetColor();
+          Dictionary<string,int> employeeOfDesignationAndJob= GetEmployeeByDepartmentIdAndDesignation();
+            foreach(var employee in employeeOfDesignationAndJob)
+            {
+                string[] designationAndDepId=employee.Key.Split('-');
+                if (employee.Value > 1)
+                {
+                    Console.ForegroundColor= ConsoleColor.Green;
+                    Console.WriteLine($"The Employee count of same ${designationAndDepId[0]} and ${designationAndDepId[1]} is {employee.Value}\n");
+                    Console.ResetColor();
+                    continue;
+                }
+                Console.WriteLine($"The Employee count of same ${designationAndDepId[0]} and ${designationAndDepId[1]} is {employee.Value}\n");
+            }
+        }
+
+        private static Dictionary<string,int> GetEmployeeByDepartmentIdAndDesignation()
+        {
+            Dictionary<string, int> employeeOfDesignationAndId = new Dictionary<string, int>();
+            using (FileStream stream = new FileStream("E:\\Swabhav Assignments\\session 30\\DataFile2.txt", FileMode.OpenOrCreate))
+            {
+                StreamReader reader = new StreamReader(stream);
+                string dataLine = reader.ReadLine();
+
+                while (dataLine != null)
+                {
+                    string[] dataLineArray = dataLine.Split(",");
+                    if (!employeeOfDesignationAndId.ContainsKey(dataLineArray[2] + "-" + dataLineArray[7]))
+                    {
+                        employeeOfDesignationAndId.Add(dataLineArray[2] +"-"+ dataLineArray[7],0);
+                    }
+                    employeeOfDesignationAndId[dataLineArray[2] + "-" + dataLineArray[7]]++;
+                    dataLine = reader.ReadLine();
+
+                    
+
+                }
+            }
+            return employeeOfDesignationAndId;
+        }
+
+        private static void CaseStudyForGeneratingUniqueRecord()
+        {
+            HashSet<string> uniqueRecord = CaseStudyOne();
+            using (FileStream stream = new FileStream("E:\\Swabhav Assignments\\session 30\\DataFile2.txt", FileMode.OpenOrCreate))
+            {
+                StreamWriter writer = new StreamWriter(stream);
+                foreach (string uniqueData in uniqueRecord)
+                {
+                    writer.WriteLine(uniqueData);
+                }
+                writer.Close();
+            }
+        }
+
+        private static HashSet<string> CaseStudyOne()
+        {
+            HashSet<string> list = new HashSet<string>();
+            using (FileStream stream = new FileStream("E:\\Swabhav Assignments\\session 30\\DataFile.txt", FileMode.OpenOrCreate))
+            {
+
+                StreamReader reader = new StreamReader(stream);
+                //StreamWriter writer=new StreamWriter(stream);
+
+                string dataLine = reader.ReadLine();
+                while (dataLine != null)
+                {
+                    list.Add(dataLine);
+                    dataLine = reader.ReadLine();
+                }
+
+
+
+            }
+            return list;
+        }
         private static void CaseStudyForGeneratingEmployingBySalary()
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("----------Maximum salary---------------\n");
+            Console.ResetColor();
             using (FileStream stream = new FileStream("E:\\Swabhav Assignments\\session 30\\DataFile2.txt", FileMode.Open))
             {
                 StreamReader reader= new StreamReader(stream);
@@ -36,14 +119,16 @@
 
                 }
                  Console.WriteLine($"{maxSalaryPersonDetails}\n\n" +
-                    $"\tThe highest salary is {maxSalary}");
+                    $"\tThe highest salary is {maxSalary}\n\n");
             }
             
         }
 
         private static void CaseStudyForGeneratingEmployingById()
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("-----------Employees By department id----------------\n");
+            Console.ResetColor();
             Dictionary<int, int> employeeById = GetEmployeeByDepartmentId();
             foreach (var employee in employeeById)
             {
@@ -94,7 +179,9 @@
 
         private static void CaseStudyForGeneratingEmployingByDesignation()
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("-----------Employees By designation----------------\n");
+            Console.ResetColor();
             Dictionary<string, int> employeeDesignation = GetEmployeeByDesignation();
             foreach (var employee in employeeDesignation)
             {
@@ -133,43 +220,6 @@
             }
             return employeeByDesignationSet;
 
-        }
-
-
-        private static void CaseStudyForGeneratingUniqueRecord()
-        {
-            HashSet<string> uniqueRecord = CaseStudyOne();
-            using (FileStream stream = new FileStream("E:\\Swabhav Assignments\\session 30\\DataFile2.txt", FileMode.OpenOrCreate))
-            {
-                StreamWriter writer = new StreamWriter(stream);
-                foreach (string uniqueData in uniqueRecord)
-                {
-                    writer.WriteLine(uniqueData);
-                }
-                writer.Close();
-            }
-        }
-
-        private static HashSet<string> CaseStudyOne()
-        {
-            HashSet<string> list = new HashSet<string>();
-            using (FileStream stream = new FileStream("E:\\Swabhav Assignments\\session 30\\DataFile.txt", FileMode.OpenOrCreate))
-            {
-
-                StreamReader reader = new StreamReader(stream);
-                //StreamWriter writer=new StreamWriter(stream);
-
-                string dataLine = reader.ReadLine();
-                while (dataLine != null)
-                {
-                    list.Add(dataLine);
-                    dataLine = reader.ReadLine();
-                }
-
-
-
-            }
-            return list;
         }
     }
 }
